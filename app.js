da18ae2a6947e00d0796d8127b387e5c579cb5cf
sriram <srirampksrirampk@gmail.com>
2025-05-17 20:45:19 +0530

    all code push to master

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express=require('express');
 const dotenv=require('dotenv');
 const bodyParser = require('body-parser');
 const authrouter=require('./routes/authRoutes');
+const cors = require('cors');
 
 dotenv.config();
 const app=express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api',authrouter);
 
